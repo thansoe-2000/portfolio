fb8b1a5e2ac11f8ea28814b9551291176d7434aa
Student <tt6289216@gmail.com>
2024-10-10 08:25:33 -0700

    add template to project

--- photo-album/djapp/myapp/urls.py
+++ photo-album/djapp/myapp/urls.py
@@ -3,7 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index_page'),
-    # path('portfolio/', views.portfolio, name='portfolio_page'),
-    # path('about/', views.about, name='about_page'),
+    # frontend urls
+    path('', views.index, name='index_page'),  #index_page
+    path('about/', views.about, name='about_page'), # about page
+    path('contact/', views.contact, name='contact_page'), #contact page
+    path('services/', views.services, name='services_page'), #services page
+    path('gallary/', views.gallary, name='gallary_page'), #gallary page
+    path('gallary_single/', views.gallary_single, name='gallary_single_page'),  #gallary single page
+    
+ 
+    
 ]
--- photo-album/djapp/myapp/views.py
+++ photo-album/djapp/myapp/views.py
@@ -6,11 +6,26 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'frontend/photo/index.html')
 
-# portfolio page
-# def portfolio(request):
-#     return render(request, 'frontend/photo/portfolio-detail.html')
-
 # about page
-# def about(request):
-#     return render(request, 'frontend/photo/about.html')
+def about(request):
+    return render(request, 'frontend/photo/about.html')
+
+# contact pages
+def contact(request):
+    return render(request, 'frontend/photo/contact.html')
+
+# services pages
+def services(request):
+    return render(request, 'frontend/photo/services.html')
+
+# gallary page
+def gallary(request):
+    return render(request, 'frontend/photo/gallary.html')
+
+# gallary single page
+def gallary_single(request):
+    return render(request, 'frontend/photo/gallary-single.html')
+
+
+
 
